835339b059be8bfe0fdfc8fd43037faa92f9aa0b
sai123-sudo <ls0122207@gmail.com>
2025-05-23 13:44:40 +0530

    hello world started

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bn/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: hey $PERSON2, How are you?"
+echo "$PERSON2: Hello $PERSON1, I am fine. How are you doing"
+echo "$PERSON1: I an fine too. What's up?"
+echo "$PERSON2: Nothing, just going to mars now, will you come?"
+echo "$PERSON1: Sorry, you carry on! I will come once you come back"
