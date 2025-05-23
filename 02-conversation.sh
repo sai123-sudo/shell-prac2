dfc75b69e5e2b591bc9b2d0001fdacdb085c8f06
sai123-sudo <ls0122207@gmail.com>
2025-05-23 13:18:25 +0530

    hello world started

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,2 +1,7 @@
-#!/bin/bash
+#!/bn/bash
 
+echo "Trump: hey musk, How are you?"
+echo "Musk: Hello Trump, I am fine. How are you doing"
+echo "Trump: I an fine too. What's up?"
+echo "Musk: Nothing, just going to mars now, will you come?"
+echo "Trump: Sorry, you carry on! I will come once you come back"
